f604df414554ab5612ac005198a2a4574ca4ab66
Anusha <anushashinde167@gmail.com>
2026-08-10 21:46:01 -0700

    main video and mini player fixed

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -29,10 +29,10 @@ const hero = () => {
         <div id="video-frame" className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75">
             <div>
                 <div className = "mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
-                    <div onClick={handleMiniVdClick} className = "origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 opacity-100">
+                    <div onClick={handleMiniVdClick} className = "origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100">
                         <video
                             ref={nextVideoRef}
-                            src={getVideoSrc(currentIndex + 1)}
+                            src={getVideoSrc(upcomingVideoIndex)}
                             loop
                             muted
                             id="current-video"
@@ -41,6 +41,25 @@ const hero = () => {
                         />
                     </div>
                 </div>
+
+                <video 
+                    ref={nextVideoRef}
+                    src={getVideoSrc(currentIndex)}
+                    loop
+                    muted
+                    id="next-video"
+                    className="absolute-center invisible absolute z-20 size-64 object-cover object-center"
+                    onLoadedData={handleVideoLoad}
+                />
+
+                <video
+                    src={getVideoSrc(currentIndex === totalVideos-1 ? 1 : currentIndex)}
+                    autoPlay
+                    loop
+                    muted
+                    className = "absolute left-0 top-0 size-full object-cover object-center"
+                />
+
             </div>
         </div>
     </div>
